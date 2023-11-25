7c9bd3385ed00e1647332a521f7002b91fed38ea
NikitaZamiatin <nikitvzamatin16@gmail.com>
2023-11-25 13:46:23 +0500

    Диалог
    Улучшил диалог

--- ugadayka.py
+++ ugadayka.py
@@ -1,21 +1,22 @@
 import random
 import logging
 
-# Настройка логгера
 logging.basicConfig(filename='game_log.txt', level=logging.INFO,
                     format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
-N = int(input("Введите число N: "))
-k = int(input("Введите число k: "))
+N = int(input("Введите число N (натуральное число): "))
+k = int(input("Введите число попыток (натуральное число): "))
 
-logging.info("Начало игры. N: %d, k: %d", N, k)
+logging.info("Начало игры. N: %d Попытки: %d", N, k)
 
 secret_number = random.randint(1, N)
 logging.info("Загаданное число: %d", secret_number)
 
+control = 0
+
 for i in range(k):
     try:
-        guess = int(input("Введите число от 1 до %d: ", N))
+        guess = int(input("Введите число от 1 до {}: ".format(N)))
         logging.info("Попытка ввода: %d", guess)
 
         if guess == secret_number:
@@ -24,15 +25,18 @@ for i in range(k):
             break
 
         if guess < secret_number:
-            print("Меньше")
-            logging.info("Игра продолжается. Результат: меньше")
+            print("Больше")
+            control += 1
+            logging.info("Игра продолжается. Результат: больше")
 
         if guess > secret_number:
-            print("Больше")
-            logging.info("Игра продолжается. Результат: больше")
+            print("Меньше")
+            control += 1
+            logging.info("Игра продолжается. Результат: меньше")
 
     except ValueError:
         print("Введите целое число!")
 
-print("Попытки закончились")
-logging.info("Игра окончена. Результат: попытки закончились")
+if control == k:
+    print("Попытки закончились. Загаданное число: {}".format(secret_number))
+    logging.info("Игра окончена. Результат: попытки закончились")
