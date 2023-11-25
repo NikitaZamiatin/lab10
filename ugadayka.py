aa0b03d7a52cc11e78e17173fe980e97e38a6a71
NikitaZamiatin <nikitvzamatin16@gmail.com>
2023-11-25 13:11:21 +0500

    Занесение информации о начале игры в логгер, выбор случайного числа, цикл для ввода пользователем чисел
    Добавил занесение информации в логгер, выбор случайного числа и цикл для ввода чисел пользователем

--- ugadayka.py
+++ ugadayka.py
@@ -8,3 +8,28 @@ logging.basicConfig(filename='game_log.txt', level=logging.INFO,
 N = int(input("Введите число N: "))
 k = int(input("Введите число k: "))
 
+logging.info("Начало игры. N: %d, k: %d", N, k)
+
+secret_number = random.randint(1, N)
+logging.info("Загаданное число: %d", secret_number)
+
+for i in range(k):
+    try:
+        guess = int(input("Введите число от 1 до %d: ", N))
+        logging.info("Попытка ввода: %d", guess)
+
+        if guess == secret_number:
+            print("Вы угадали!")
+            logging.info("Игра окончена. Результат: угадали")
+            break
+
+        if guess < secret_number:
+            print("Меньше")
+            logging.info("Игра продолжается. Результат: меньше")
+
+        if guess > secret_number:
+            print("Больше")
+            logging.info("Игра продолжается. Результат: больше")
+
+    except ValueError:
+        print("Введите целое число!")
